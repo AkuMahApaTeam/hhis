99f81e8d6cdcb6c756675392216f1969ba2fd8df
abdullah.ainun4 <abdullah.ainun4@gmail.com>
2017-11-07 09:12:58 +0700

    id_user sebagai parameter riwayat

--- api/modules/v1/controllers/RiwayatController.php
+++ api/modules/v1/controllers/RiwayatController.php
@@ -37,7 +37,11 @@ class RiwayatController extends Controller
         ];
     }
 
-    public function actionView($id_pasien){
+    public function actionView($id_user){
+        $id_pasien = \api\modules\v1\models\Pasien::find()
+            ->select('id_pasien')
+            ->where(['id_user' => $id_user])
+            ->one();
         $id_dokter = \api\modules\v1\models\Riwayat::find()
             ->select('id_dokter')
             ->where(['id_pasien' => $id_pasien])
@@ -90,10 +94,6 @@ class RiwayatController extends Controller
             ->select('larangan')
             ->where(['id_pasien' => $id_pasien])
             ->all();
-        $note = \api\modules\v1\models\Riwayat::find()
-            ->select('note')
-            ->where(['id_pasien' => $id_pasien])
-            ->all();
         $tgl_periksa= \api\modules\v1\models\Riwayat::find()
             ->select('tgl_periksa')
             ->where(['id_pasien' => $id_pasien])
@@ -127,9 +127,14 @@ class RiwayatController extends Controller
                 'keluhan_utama' => $keluhan_utama[0]->keluhan_utama,
                 'diagnosa' => $diagnosa[0]->nama_penyakit,
                 'larangan' => $larangan[0]->larangan,
-                'note' => $note[0]->note,
                 'tgl_periksa' => $tgl_periksa[0]->tgl_periksa,
-                'perawatan' => $perawatan[0]->perawatan
+                'perawatan' => $perawatan[0]->perawatan,
+                'advis' => $model->advis,
+                'head' => $model->head,
+                'neck' => $model->neck,
+                'thorax' => $model->thorax,
+                'abdomen' => $model->abdomen,
+                'ekstremitas' => $model->ekstremitas
             ];
         }
         return [
